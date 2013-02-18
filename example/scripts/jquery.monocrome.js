b8a143caa00df95e5d44a017df88dd3468724e08
Jepser <jepsersk8@gmail.com>
2013-02-17 20:25:49 -0600

    Update plugin

--- example/scripts/jquery.monocrome.js
+++ example/scripts/jquery.monocrome.js
@@ -29,48 +29,64 @@
 		
 		return this.each(function(index,wrap){
 			
-
 			
-			if ( options ) { 
-				settings = $.extend( settings, options );
-			}
-			
-			var pic, picWidth, picHeight, currCanvas;
-			
-			var noWrap = settings.isImage, hide = settings.hideOriginal;
-			
-			if(noWrap == true){
-				pic = this;
-				picWidth  = $(pic).prop('width');
-				picHeight = $(pic).prop('height');
-				$('<canvas width="' + picWidth + '" height="' +picHeight + '"></canvas>').insertAfter(pic);
-				currCanvas = $(this).next('canvas');
-			} else {
-				pic = $(wrap).find('img');
-				for(var a = 0; a < pic.length; a++){
-					picWidth  = $(pic).eq(a).prop('width');
-					picHeight = $(pic).eq(a).prop('height');
-					$('<canvas width="' + picWidth + '" height="' +picHeight + '"></canvas>').appendTo(wrap);
+				if ( options ) { 
+					settings = $.extend( settings, options );
 				}
-				currCanvas = $(wrap).find('canvas');
-			}
-			if(hide){
-				$(pic).remove();
-			}
-			if(!picHeight || !picWidth){
-				alert('There has been an error...');
-			}
+				
+				var pic, picWidth, picHeight, currCanvas;
+				
+				var noWrap = settings.isImage, hide = settings.hideOriginal;
 			
-			//adding the canvas
-			if(noWrap != true){
-				for(var a = 0; a < pic.length; a++){
-					grayImages(pic[a],currCanvas[a],picWidth,picHeight);
+				
+				if(noWrap == true){
+					pic = this;
+					picWidth  = $(pic).prop('width');
+					picHeight = $(pic).prop('height');
+					if(picWidth == 0){
+						picWidth  = $(this).width();
+					}
+					if(picHeight == 0){
+						picHeight = $(this).height();
+					}
+					$('<canvas width="' + picWidth + '" height="' +picHeight + '"></canvas>').insertAfter(pic);
+					currCanvas = $(this).next('canvas');
+				} else {
+					pic = $(wrap).find('img');
+					$.each(pic, function(){
+						picWidth  = $(this).prop('width');
+						picHeight = $(this).prop('height');
+						if(picWidth == 0){
+							picWidth  = $(this).width();
+						}
+						if(picHeight == 0){
+							picHeight = $(this).height();
+						}
+						
+						//console.log(picWidth);
+						//console.log(picHeight);
+						
+						$('<canvas width="' + picWidth + '" height="' +picHeight + '"></canvas>').appendTo(wrap);
+						if(hide){
+							$(this).remove();
+						}
+					});
+					currCanvas = $(wrap).find('canvas');
 				}
-			} else {
-				grayImages(pic,currCanvas[0],picWidth,picHeight);
-			}
+				if(!picHeight || !picWidth){
+					alert('There has been an error...');
+				}
+				
+				//adding the canvas
+				if(noWrap != true){
+					for(var a = 0; a < pic.length; a++){
+						grayImages(pic[a],currCanvas[a],picWidth,picHeight);
+					}
+				} else {
+					grayImages(pic,currCanvas[0],picWidth,picHeight);
+				}
 			
 		});
 	
-	  };
+	  }; //$.fn.monocrome
 	})( jQuery );
